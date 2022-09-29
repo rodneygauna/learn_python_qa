1209ed8e3f57402ffcd2e5503ab52e38beb70223
Rodney Gauna <rodneygauna@gmail.com>
2022-09-28 23:09:36 -0700

    better formatting

--- features/steps/about_us_steps.py
+++ features/steps/about_us_steps.py
@@ -11,12 +11,12 @@ def navigate_to_webdevable(context):
 
 @when('clicks the ABOUT US link in the navigation bar')
 def navigate_to_about_us(context):
-    context.driver.find_element(By.XPATH,
-                                '/html/body/nav/div/div[2]/ul/li[2]/a').click()
+    context.driver.find_element(
+        By.XPATH, '/html/body/nav/div/div[2]/ul/li[2]/a').click()
 
 
 @then('the About Us title appears')
 def about_us_title(context):
-    title = context.driver.find_element(By.XPATH,
-                                        '/html/body/div/div/div[1]/div/div/h1')
+    title = context.driver.find_element(
+        By.XPATH, '/html/body/div/div/div[1]/div/div/h1')
     assert title.is_displayed
